22220caa175fb2f0dd86762c9058c1683e581f7d
JnBenavides <julianbenavides9@gmail.com>
2026-08-19 19:39:13 -0500

    Add files via upload

---
+++ MiClase.java
@@ -0,0 +1,16 @@
+public class MiClase{
+    private int edad;
+    private double altura;
+    private char sexo;
+    edad = 27;
+    altura = 170;
+    sexo = "M";
+    private boolean aprobado = true;
+    String nombre = "Julian Benavides";
+    String saludo = "Hola" + nombre;
+    double precio = 9.99;
+    int precioCortado = (int)precio;
+    int entero = 5;
+    double decimal = entero;
+    
+}
